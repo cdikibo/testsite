529e8d1ca1a6f5b1ad792caea6c4ded46b42ed58
cdikibo <cdikibo@alumni.nd.edu>
2018-05-07 12:06:33 -0500

    inital commit

---
+++ meta/config.js
@@ -0,0 +1,22 @@
+/**
+ * Created by vaibhav on 31/3/18
+ */
+module.exports = {
+  siteTitle: 'Caroline Dikibo', // Site title.
+  siteTitleAlt: 'Caroline Dikibo', // Alternative site title for SEO.
+  siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://carolinedikibo.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'All about Caroline', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'caroline', // Disqus shortname.
+  userName: 'Caroline Dikibo',
+  userTwitter: 'carofine247',
+  userLocation: 'Houston, TX',
+  userDescription: '',
+  copyright: 'Copyright © Caroline Dikibo 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
